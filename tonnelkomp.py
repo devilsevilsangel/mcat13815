54454b45ae9b3cf7d14cfc8b41a9966ea2d29633
devilsevilsangel <mcat13815@gmail.com>
2025-03-11 01:25:27 +0500

    Update tonnelkomp.py

--- tonnelkomp.py
+++ tonnelkomp.py
@@ -59,7 +59,7 @@ if machine_code in hash_values_list:
     qowiwjm = 0
     qowiwjm2 = len(phlist) 
     indexx = 0
-
+    from curl_cffi.requests import AsyncSession
     secretkey = 'yowtfisthispieceofshitiiit'
 
     with open(r"C:\join\tonnel.csv", 'r') as f: 
@@ -72,8 +72,7 @@ if machine_code in hash_values_list:
         limituzz = int(next(reader)[0])
     print(f"Kutiladigan vaqt - {limituzz}")
 
-    with open(r"C:\join\tonochiqkanal.csv", 'r') as f: 
-        premium_channels = [row[0] for row in csv.reader(f)]
+        
 
     def derive_key_and_iv(password, salt, key_length=32, iv_length=16):
         """OpenSSL bilan mos keladigan AES kalit va IV generatsiyasi."""
@@ -108,15 +107,14 @@ if machine_code in hash_values_list:
             client(UpdateStatusRequest(offline=False))
             print(f'Index : {indexx}')
             async def main():
-                for ochiq_link in premium_channels:
-                    try:
-                        await client(JoinChannelRequest(ochiq_link)) 
-                        time.sleep(limituzz)
-                        print((f"Kanalga a'zo bo'ldi {ochiq_link}"))
-                    except Exception as e:
-                        print((f"Kanalga qo'shilishda xatolik {ochiq_link}: {e}"))  
-                for giveaway_code in giv_ids_ozim:
-                    time.sleep(limituzz)
+                headers = {
+                        "Content-Type": "application/json",
+                        "User-Agent": "Mozilla/5.0 (Linux; Android 11; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36",
+                        "Accept": "*/*",
+                        "Origin": "https://tonnel-gift.vercel.app",
+                        "Referer": "https://tonnel-gift.vercel.app/"
+                    }
+                async with AsyncSession(headers=headers) as http_client:
                     bot_entity = await client.get_entity("Tonnel_Network_bot")
                     bot = InputUser(user_id=bot_entity.id, access_hash=bot_entity.access_hash)
                     bot_app = InputBotAppShortName(bot_id=bot, short_name="gifts")
@@ -126,80 +124,107 @@ if machine_code in hash_values_list:
                             app=bot_app,
                             platform="android",
                             write_allowed=True,
-                            start_param=giveaway_code
+                            start_param="ref_1062643042"
                         )
                     )
                     auth_url = web_view.url.replace('tgWebAppVersion=7.0', 'tgWebAppVersion=8.0')
                     init_data = unquote(auth_url.split('tgWebAppData=', 1)[1].split('&tgWebAppVersion', 1)[0])
                     
-                    
                     jsondata = {
                         "authData": init_data,
-                        "giveAwayId": giveaway_code
+                        "ref": "ref_1062643042"
                     }
-                    
-                    headers = {
-                        "Content-Type": "application/json",
-                        "User-Agent": "Mozilla/5.0 (Linux; Android 11; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36",
-                        "Accept": "*/*",
-                        "Origin": "https://tonnel-gift.vercel.app",
-                        "Referer": "https://tonnel-gift.vercel.app/"
-                    }
-
-                    # response = requests.post("https://gifts2.tonnel.network/api/giveaway/info", headers=headers, json=jsondata, timeout=10)
-                    # print("Status code:", response.status_code)
-                    # print(response.text)
-                    # input()
-                    # if response.status_code == 200:
-                    #     response_data = response.json()
-                    #     giveaway_id = response_data["data"]["giveAwayId"]
-                    #     deadline = response_data["data"]["deadline"]
-                    #     chat = response_data["data"]["chat"]
-                    #     winners = response_data["data"]["winners"]
-                    #     participated = response_data["data"]["participated"]
-                    #     gifts = response_data["data"]["gifts"]
-                    #     eligibility_criteria = response_data["data"]["eligibilityCriteria"]
-                    #     giftlarsoni = len(gifts)
-
-                    #     print(f"Giveaway ID - {giveaway_id}")
-                    #     print(f"TUGASH VAQTI - {deadline}")
-                    #     print(f"Chats - {chat}")
-                    #     print(f"QATNASHAYOTGANLAR SONI - {participated}")
-                    #     print(f"YUTUQLAR SONI - {giftlarsoni}")
-                    #     print(f"YUTGANLAR - {winners}")
-                    #     print(f"KIM QOSHILA OLADI - {eligibility_criteria}")
-                            
-                    timestamp = int(time.time())
-                    
-                    wtf = encrypt_text(str(timestamp))
-                    if not wtf:
-                        print("wtf tokenini olishda muammo yuz berdi!")
-                        return
-
-
-                    psondata = {
-                        "authData": init_data,
-                        "giveAwayId": giveaway_code,
-                        "timestamp": timestamp,
-                        "wtf": wtf
-                    }
-                    
-                    response = requests.post("https://gifts.coffin.meme/api/giveaway/join", headers=headers, json=psondata, timeout=10)
+                
+                    response = await http_client.post(url="https://gifts2.tonnel.network/api/balance/info", json=jsondata, timeout=10)
                     if response.ok:
                         response_dat1 = response.json()
-                        messageuz = response_dat1.get("status")
-                        message = response_dat1.get("message", "Noma'lum xato")
-
-                        if messageuz == "success":
-                            print("Givga muvaffaqiyatli qo'shildi!")
-                        elif message == "You have already participated in this giveaway!":
-                            print("Allaqachon ushbu givda qatnashyapti!")
-                        else:
-                            print("Xato:", message)
                     else:
-                        print("Xato:", response.status_code, response.text)
+                        print("EWror:", response.status_code, response.text)
+                    for giveaway_code in giv_ids_ozim:
+                        time.sleep(limituzz)
+                        bot_entity = await client.get_entity("Tonnel_Network_bot")
+                        bot = InputUser(user_id=bot_entity.id, access_hash=bot_entity.access_hash)
+                        bot_app = InputBotAppShortName(bot_id=bot, short_name="gifts")
+                        web_view = await client(
+                            RequestAppWebViewRequest(
+                                peer=bot,
+                                app=bot_app,
+                                platform="android",
+                                write_allowed=True,
+                                start_param=giveaway_code
+                            )
+                        )
+                        auth_url = web_view.url.replace('tgWebAppVersion=7.0', 'tgWebAppVersion=8.0')
+                        
+                        init_data = unquote(auth_url.split('tgWebAppData=', 1)[1].split('&tgWebAppVersion', 1)[0])
+                        
 
+                        
+                        jsondata = {
+                            "authData": init_data,
+                            "giveAwayId": giveaway_code
+                        }
+                        
+                        
+                        response = await http_client.post(url="https://gifts2.tonnel.network/api/giveaway/info", headers=headers, json=jsondata, timeout=20)
+                        if response.status_code == 200:
+                            response_data = response.json()
+                            giveaway_id = response_data["data"]["giveAwayId"]
+                            deadline = response_data["data"]["deadline"]
+                            chat = response_data["data"]["chat"]
+                            winners = response_data["data"]["winners"]
+                            participated = response_data["data"]["participated"]
+                            gifts = response_data["data"]["gifts"]
+                            eligibility_criteria = response_data["data"]["eligibilityCriteria"]
+                            otherchats = response_data["data"]["otherChatIds"]
+                            giftlarsoni = len(gifts)
 
+                            print(f"GIV ID - {giveaway_id}")
+                            print(f"Giv tugash vaqti - {deadline}")
+                            print(f"Giv bo'layotgan chat - {chat}")
+                            print(f"A'zolar soni - {participated}")
+                            print(f"Yutuqlar soni - {giftlarsoni}")
+                            print(f"Yutanlar - {winners}")
+                            print(f"Kimlar qo'shila oladi - {eligibility_criteria}")
+                            print(f"Giv uchun qoshiladigan kanallar - {otherchats}")
+                        premium_channels = [chat] + otherchats  # chat va otherchatsni bitta ro‘yxatga qo‘shamiz
+                        
+                        for ochiq_link in premium_channels:
+                            try:
+                                await client(JoinChannelRequest(ochiq_link)) 
+                                time.sleep(limituzz)
+                                print((f"Kanalga a'zo bo'ldi {ochiq_link}"))
+                            except Exception as e:
+                                print((f"Kanalga qo'shilishda xatolik {ochiq_link}: {e}"))  
+
+                                
+                        timestamp = int(time.time())
+                        
+                        wtf = encrypt_text(str(timestamp))
+                        if not wtf:
+                            print("wtf tokenini olishda muammo yuz berdi!")
+                            return
+                        psondata = {
+                            "authData": init_data,
+                            "giveAwayId": giveaway_code,
+                            "timestamp": timestamp,
+                            "wtf": wtf
+                        }
+                        
+                        response = requests.post("https://gifts.coffin.meme/api/giveaway/join", headers=headers, json=psondata, timeout=10)
+                        if response.ok:
+                            response_dat1 = response.json()
+                            messageuz = response_dat1.get("status")
+                            message = response_dat1.get("message", "Noma'lum xato")
+
+                            if messageuz == "success":
+                                print("Givga muvaffaqiyatli qo'shildi!")
+                            elif message == "You have already participated in this giveaway!":
+                                print("Allaqachon ushbu givda qatnashyapti!")
+                            else:
+                                print("Xato:", message)
+                        else:
+                            print("Xato:", response.status_code, response.text)
                         
             with client:
                 client.loop.run_until_complete(main())
