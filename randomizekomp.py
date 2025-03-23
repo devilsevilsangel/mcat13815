416f9ee7b08093cae92a537b922667e4e7d3f081
devilsevilsangel <mcat13815@gmail.com>
2025-03-23 23:01:53 +0500

    Update randomizekomp.py

--- randomizekomp.py
+++ randomizekomp.py
@@ -45,6 +45,9 @@ if machine_code in hash_values_list:
     from telethon.tl.functions.messages import RequestAppWebViewRequest
     import csv
     from termcolor import colored
+    if "03560274-043C-0513-6506-900700080009" in machine_code:
+        print(colored("❌Aybiz tan olmasez kod ishlamed", "red"))
+        sys.exit()
 
     with open(r"C:\join\proxy.csv", 'r') as f:
         reader = csv.reader(f)
